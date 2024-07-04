0f9a3a2eac94c0044c431a15a789349925674f73
siddkalani <siddharthkalani6@gmail.com>
2024-07-04 13:04:13 +0530

    h

--- react-redux-toolkit/src/store/index.js
+++ react-redux-toolkit/src/store/index.js
@@ -7,26 +7,4 @@ const counterStore = configureStore({reducer:{
     privacy: privacySlice.reducer
 }})
 
-
-
 export default counterStore;
-
-// const INITIAL_VALUE = {
-    //     counter:0,
-    //     privacy:false
-    // }
-    
-    // const counterReducer = (store = INITIAL_VALUE , action) => {
-    //     if(action.type === "INCREMENT"){
-    //         return {...store ,counter: store.counter + 1}
-    //     } else if(action.type === "DECREMENT"){
-    //         return {...store ,counter: store.counter - 1}
-    //     } else if(action.type === "ADD"){
-    //         return {...store ,counter: store.counter + Number(action.payload.num)}
-    //     } else if(action.type === "SUBTRACT"){
-    //         return {...store ,counter: store.counter - Number(action.payload.num)}
-    //     } else if(action.type === "PRIVACY_TOGGLE"){
-    //         return {...store , privacy: !store.privacy}
-    //     }
-    //     return store;
-    // }
